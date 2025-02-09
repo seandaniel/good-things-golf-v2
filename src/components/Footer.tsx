6d42a452c6c4553f18b65c410166329cd281d426
seandaniel <degroot.sd@gmail.com>
2025-02-09 08:17:49 -0500

    Fix: OB image, footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ export const Footer = () => {
       <div className="wrapper wrapper-footer"></div>
       <div className="wrapper">
         <h3>
-          All images courtesy of SdG<br></br>(steal them and nothing will
-          happen)
+          All images courtesy of Sean de Groot<br></br>(steal them and nothing
+          will happen)
         </h3>
       </div>
     </footer>
